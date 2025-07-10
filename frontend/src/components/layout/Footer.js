c9d919ed89a93cc92df3e315c5f53484c280e326
DhanushPadarthi <padarthidhanush@gmail.com>
2025-07-10 09:49:13 +0530

    j

--- frontend/src/components/layout/Footer.js
+++ frontend/src/components/layout/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => (
   <footer className="bg-dark text-white p-3 mt-auto">
     <div className="container text-center fs-6">
-      &copy; 2023 ShopSmart. All rights reserved. | Built with HTML, CSS (Bootstrap), & JavaScript.
+      &copy; 2023 ShopSmart. All rights reserved.
     </div>
   </footer>
 );
